704b9828a51ad7c719150cce0789aa0952dec182
e <751232478@qq.com>
2020-03-20 18:48:20 +0800

    首页布局完成-差热词

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,77 +1,268 @@
 <template>
   <el-container class="" style="width:100%;text-align:center;">
     <header-all></header-all>
-    <el-main>
-    </el-main>
+     <el-container style="width:80%;margin-left:10%;" >
+      <el-main style="">
+        <el-carousel :interval="5000" arrow="always" style="height:450px;width:100%;" >
+        <el-carousel-item style="height:450px" v-for="(item,index) in imgs" :key="index">
+          <img :src="item" style="width:100%;height:100%;" alt="">
+        </el-carousel-item>
+      </el-carousel>
+      <el-card class="hot-card" style="text-align:left;margin-top:20px">
+        <div slot="header" class="clearfix">
+          <span style="font-size:28px;font-weight:700"><i style="color:#545c64;font-size:32px" class="el-icon-s-promotion"></i>最热推荐</span>
+          <el-button style="float: right; padding: 3px 0;font-size:30px" type="text">>></el-button>
+        </div>
+         <div class="common_news_list">
+                <a href="#" class="list_pic"><img src="../../assets/img/pic09.jpg" alt=""></a>
+                <h4><a href="#">亚马逊金融试水中国？背后却是和华尔街的“金融战争” </a></h4>
+                <p>亚马逊抛弃了中国的买家们，却难以割舍庞大的卖家市场，有消息指出亚马逊中国将针对中国庞大的卖家群体推出一项贷款推荐计划。在连续对印度、墨西哥等新兴市场大力推广线上支付服务后，亚马逊金融试水进军中国市场的信号终于被放出。
+                </p>
+                <div class="new_info">
+                    <span>标签：财经&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<i class="el-icon-view"></i>&nbsp;阅读( 5
+                        )&nbsp;&nbsp;&nbsp;<i class="el-icon-chat-dot-round"></i>&nbsp;评论( 10 )</span>
+                    <b>发布于 2018-08-30 12:18:58</b>
+                </div>
+          </div>
+            <el-divider></el-divider>
+            <div class="common_news_list">
+                <a href="#" class="list_pic"><img src="../../assets/img/pic03.jpg" alt=""></a>
+                <h4><a href="#">马斯克的新花样？连接人脑和电脑的初创公司又筹了一笔钱,马斯克的新花样？连接人脑和电脑的初创公司又筹了一笔钱</a></h4>
+                <p>亚马逊抛弃了中国的买家们，却难以割舍庞大的卖家市场，有消息指出亚马逊中国将针对中国庞大的卖家群体推出一项贷款推荐计划。在连续对印度、墨西哥等新兴市场大力推广线上支付服务后，亚马逊金融试水进军中国市场的信号终于被放出。
+                </p>
+                <div class="new_info">
+                    <span>标签：财经&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<i class="el-icon-view"></i>&nbsp;阅读( 5
+                        )&nbsp;&nbsp;&nbsp;<i class="el-icon-chat-dot-round"></i>&nbsp;评论( 10 )</span>
+                    <b>发布于 2018-08-30 12:18:58</b>
+                </div>
+            </div>
+             <div class="common_news_list">
+                <a href="#" class="list_pic"><img src="../../assets/img/pic09.jpg" alt=""></a>
+                <h4><a href="#">亚马逊金融试水中国？背后却是和华尔街的“金融战争” </a></h4>
+                <p>亚马逊抛弃了中国的买家们，却难以割舍庞大的卖家市场，有消息指出亚马逊中国将针对中国庞大的卖家群体推出一项贷款推荐计划。在连续对印度、墨西哥等新兴市场大力推广线上支付服务后，亚马逊金融试水进军中国市场的信号终于被放出。
+                </p>
+                <div class="new_info">
+                    <span>标签：财经&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<i class="el-icon-view"></i>&nbsp;阅读( 5
+                        )&nbsp;&nbsp;&nbsp;<i class="el-icon-chat-dot-round"></i>&nbsp;评论( 10 )</span>
+                    <b>发布于 2018-08-30 12:18:58</b>
+                </div>
+          </div>
+            <el-divider></el-divider>
+            <div class="common_news_list">
+                <a href="#" class="list_pic"><img src="../../assets/img/pic03.jpg" alt=""></a>
+                <h4><a href="#">马斯克的新花样？连接人脑和电脑的初创公司又筹了一笔钱,马斯克的新花样？连接人脑和电脑的初创公司又筹了一笔钱</a></h4>
+                <p>亚马逊抛弃了中国的买家们，却难以割舍庞大的卖家市场，有消息指出亚马逊中国将针对中国庞大的卖家群体推出一项贷款推荐计划。在连续对印度、墨西哥等新兴市场大力推广线上支付服务后，亚马逊金融试水进军中国市场的信号终于被放出。
+                </p>
+                <div class="new_info">
+                    <span>标签：财经&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<i class="el-icon-view"></i>&nbsp;阅读( 5
+                        )&nbsp;&nbsp;&nbsp;<i class="el-icon-chat-dot-round"></i>&nbsp;评论( 10 )</span>
+                    <b>发布于 2018-08-30 12:18:58</b>
+                </div>
+            </div>
+             <div class="common_news_list">
+                <a href="#" class="list_pic"><img src="../../assets/img/pic09.jpg" alt=""></a>
+                <h4><a href="#">亚马逊金融试水中国？背后却是和华尔街的“金融战争” </a></h4>
+                <p>亚马逊抛弃了中国的买家们，却难以割舍庞大的卖家市场，有消息指出亚马逊中国将针对中国庞大的卖家群体推出一项贷款推荐计划。在连续对印度、墨西哥等新兴市场大力推广线上支付服务后，亚马逊金融试水进军中国市场的信号终于被放出。
+                </p>
+                <div class="new_info">
+                    <span>标签：财经&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<i class="el-icon-view"></i>&nbsp;阅读( 5
+                        )&nbsp;&nbsp;&nbsp;<i class="el-icon-chat-dot-round"></i>&nbsp;评论( 10 )</span>
+                    <b>发布于 2018-08-30 12:18:58</b>
+                </div>
+          </div>
+            <el-divider></el-divider>
+            <div class="common_news_list">
+                <a href="#" class="list_pic"><img src="../../assets/img/pic03.jpg" alt=""></a>
+                <h4><a href="#">马斯克的新花样？连接人脑和电脑的初创公司又筹了一笔钱,马斯克的新花样？连接人脑和电脑的初创公司又筹了一笔钱</a></h4>
+                <p>亚马逊抛弃了中国的买家们，却难以割舍庞大的卖家市场，有消息指出亚马逊中国将针对中国庞大的卖家群体推出一项贷款推荐计划。在连续对印度、墨西哥等新兴市场大力推广线上支付服务后，亚马逊金融试水进军中国市场的信号终于被放出。
+                </p>
+                <div class="new_info">
+                    <span>标签：财经&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<i class="el-icon-view"></i>&nbsp;阅读( 5
+                        )&nbsp;&nbsp;&nbsp;<i class="el-icon-chat-dot-round"></i>&nbsp;评论( 10 )</span>
+                    <b>发布于 2018-08-30 12:18:58</b>
+                </div>
+            </div>
+
+      </el-card>
+      </el-main>
+      <el-aside width="300px" style="margin-top:20px">
+        <el-card style="width:99%;height:450px">
+          <el-tabs v-model="activeName" type="card" @tab-click="handleClick">
+            <el-tab-pane label="微博热搜" name="first">
+              <iframe src="https://s.weibo.com/top/summary" width="90%" frameborder="0"></iframe>
+            </el-tab-pane>
+            <el-tab-pane label="知乎热搜" name="second">配置管理</el-tab-pane>
+          </el-tabs>
+        </el-card>
+        <el-card class="comm-card" style="text-align:left;margin-top:20px;padding:0">
+          <div slot="header" class="clearfix" >
+              <span style="font-size:20px;font-weight:700"><i style="color:#545c64;font-size:20px" class="el-icon-s-comment"></i>最新评论</span>
+          </div>
+          <ul class="content_list comment_list">
+                    <li>
+                        <span>山</span>
+                        <b><em>山治</em> 9个月前(08-14)说:</b>
+                        <strong>挺会玩的</strong>
+                    </li>
+                    <li>
+                        <span>丽</span>
+                        <b><em>丽丽</em> 9个月前(08-14)说:9个月前(08-14)说:9个月前(08-14)说:</b>
+                        <strong>挺会玩的挺会玩的挺会玩的挺会玩的</strong>
+                    </li>
+                    <li>
+                        <span>R</span>
+                        <b><em>Rechael</em> 9个月前(08-14)说:</b>
+                        <strong>挺会玩的</strong>
+                    </li>
+                    <li>
+                        <span>J</span>
+                        <b><em>John</em> 9个月前(08-14)说:</b>
+                        <strong>挺会玩的</strong>
+                    </li>
+                    <li>
+                        <span>段</span>
+                        <b><em>段正淳</em> 9个月前(08-14)说:</b>
+                        <strong>挺会玩的</strong>
+                    </li>
+                    <li>
+                        <span>乔</span>
+                        <b><em>乔峰</em> 9个月前(08-14)说:</b>
+                        <strong>挺会玩的</strong>
+                    </li>
+                </ul>
+        </el-card>
+        <el-card class="hotwords-card" style="text-align:left;margin-top:20px">
+          <div slot="header" class="clearfix">
+              <span style="font-size:20px;font-weight:700"><i style="color:#545c64;font-size:20px" class="el-icon-loading"></i>热词星球</span>
+          </div>
+          <div v-for="o in 4" :key="o" class="text item">
+            {{'列表内容 ' + o }}
+          </div>
+        </el-card>
+      </el-aside>
+    </el-container>
     <el-footer style="margin:0;padding:0">
       <footer-all></footer-all>
     </el-footer>
   </el-container>
 </template>
 <script>
-import { getStyleChannels, getAddresChannels } from '@/api/channel.js'
 
 export default {
   name: 'home',
   components: {},
   data () {
     return {
-      activeIndex: '1',
-      stylechannel: [],
-      addresschannel: [],
-      hotfilm: ['安家', '完美关系', '三生三世十里桃花--枕上书', '想见你'],
-      newsa: ['papi酱生子', '外交部 坚决反对称新冠病毒是中国病毒', '英国再现集装箱藏人案', '甘薇放弃财产优先分配权'],
-      newsb: ['4128万多名党员捐款47.3亿元', '迪丽热巴翻牌', '中国科研团队发现新冠病毒已突变', '新冠肺炎与季节性流感有4大不同']
+      imgs: ['https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1584691787583&di=4e48cc0b077255c1f61e4b7ba9b20965&imgtype=0&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20200227%2Feed4fc377a514e50a382e64849131946.jpeg',
+        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1584692609687&di=f904d7e1d4821b27c760c1d2aa0cf4ea&imgtype=0&src=http%3A%2F%2Fimg.pconline.com.cn%2Fimages%2Fupload%2Fupc%2Ftx%2Fwallpaper%2F1207%2F09%2Fc1%2F12275680_1341814069050.jpg',
+        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1584692649184&di=eb0cd0c9b17c7a876cd1191bb5b0a0a8&imgtype=0&src=http%3A%2F%2Fpcs4.clubstatic.lenovo.com.cn%2Fdata%2Fattachment%2Fforum%2F201601%2F29%2F090020nejbgt8ckgefjxcc.jpg',
+        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1584692432073&di=24f2d937ba25c982312af147bdaa55a2&imgtype=0&src=http%3A%2F%2Fdimg02.c-ctrip.com%2Fimages%2Ffd%2Ftg%2Fg2%2FM02%2F8B%2F33%2FCghzf1Ww3rCAKMvTABUl9T2-0U8651_R_1600_10000_Mtg_7.jpg']
     }
   },
   methods: {
-    handleSelect (key, keyPath) {
-      this.$store.state.selectindex = key
-    },
-    async loadStyleChannels () {
-      const { data } = await getStyleChannels()
-      this.stylechannel = data.data.items
-    },
-    godetails (id) {
-      this.$store.state.keyid = id
-      this.$router.push('/details')
-    },
-    // 获取时下新闻
-    async getnewsall () {
-      // 调用删除接口
-      const { data } = await this.$axios({
-        method: 'get',
-        url: `https://www.enlightent.cn/research/top/getWeiboRank.do?type=realTimeHotSearchList`,
-        headers: {
-          'Content-Type': 'application/json', // 设置请求头请求格式为JSON
-          'access_token': this.token // 设置token 其中K名要和后端协调好
-        }
-      })
-      this.newsa = data.slice(1, 5)
-      this.newsb = data.slice(-1, -5)
-    },
-    async loadAddresChannels () {
-      const { data } = await getAddresChannels()
-      this.addresschannel = data.data.items
-    },
-    tosomewhere () {
-      if (window.localStorage.getItem('isAdmin') === 'ADMIN') {
-        this.$router.push('/account')
-      } else {
-        this.$router.push('/visitor')
-      }
-    }
-    // load () {
-    //   this.count += 2
-    // }
   },
   created () {
-    this.loadStyleChannels()
-    this.loadAddresChannels()
-    // this.getnewsall()
   }
 }
 </script>
 <style lang="less" scoped>
+  .common_news_list {
+    height: 135px;
+    padding-left: 195px;
+    position: relative;
+    margin-top: 20px;
+    padding-bottom: 20px;
+    border-bottom: 1px solid #ddd;
+    overflow: hidden;
+      .list_pic {
+      position: absolute;
+      width: 180px;
+      height: 135px;
+      left: 0;
+      top: 0;
+        .common_news_list .list_pic img {
+        width: 180px;
+        height: 135px;
+        }
+      }
+      h4 {
+          font-size: 18px;
+          line-height: 20px;
+          font-weight: bold;
+          width: 100%;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+          margin-top: -1px;
+        }
+       p {
+          font-size: 12px;
+          color: #666;
+          line-height: 22px;
+          margin-top: 12px;
+      }
+}
+.new_info {
+    overflow: hidden;
+    margin-top: 20px;
+   span {
+        font-size: 12px;
+        color: #999;
+        float: left;
+    }
+    b {
+        font-size: 12px;
+        color: #999;
+        float: right;
+        font-weight: normal;
+    }
+}
+ul {
+    list-style: none;
+    padding:0;
+    margin:0;
+}
+.content_list {
+    margin: 5px 15px;
+     li {
+      height: 59px;
+      border-bottom: 1px dashed #e6e6e6;
+      overflow: hidden;
+       span {
+          float: left;
+          width: 18px;
+          height: 18px;
+          background-color: #999999;
+          text-align: center;
+          line-height: 18px;
+          color: #fff;
+          font-size: 12px;
+          margin-top: 9px;
+      }
+      span {
+          width: 40px;
+          height: 40px;
+          line-height: 40px;
+          font-size: 20px;
+          background-color: #545c64;
+          border-radius: 50%;
+          margin: 10px 10px 0 0;
+      }
+       b, strong {
+          font-size: 12px;
+          font-weight: normal;
+          float: left;
+          width: 168px;
+          color: #999;
+          margin-top: 10px;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+      }
+      strong {
+          margin-top: 5px;
+      }
+    }
+}
 
 </style>
